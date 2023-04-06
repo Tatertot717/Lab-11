4e04f4b1d73f48949c4d6201079e062662750d5f
Tate J Smith <tatejsmith@bellsouth.net>
2023-04-06 09:00:04 -0400

    final

--- Program.cs
+++ Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Baby[] Babies = new Baby[5];
+            Babies[0] = new Baby("Noah");
+            Babies[1] = new Baby("Olivia");
+            Babies[2] = new Baby("Liam");
+            Babies[3] = new Baby("Emma");
+            Babies[4] = new Baby("Amelia");
+
+            foreach (var item in Babies)
+            {
+                Thread b = new(item.Run);
+                b.Start();
+            }
         }
     }
 
@@ -13,10 +24,21 @@
         int time;
         string name;
 
-        Baby(string name) 
+        public Baby(string name) 
         {
             this.name = name;
-
+            Random rand = new Random();
+            time = rand.Next(5000);
+        }
+        public void Run() 
+        {
+            try
+            {
+                Console.WriteLine("My name is " + name + " I am going to sleep for " + time + "ms");
+                Thread.Sleep(time);
+                Console.WriteLine("My name is " + name + "  and I'm awake,feed me!!!");
+            }
+            catch(ThreadInterruptedException e) { Console.WriteLine(e.Message); };
         }
     }
 }
