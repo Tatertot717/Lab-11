c35dec56746dea92210b6e3153f58ef1d1042531
Tate J Smith <tatejsmith@bellsouth.net>
2023-04-06 08:28:41 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Lab_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public class Baby
+    {
+        int time;
+        string name;
+
+        Baby(string name) 
+        {
+            this.name = name;
+
+        }
+    }
+}
